e643ad225800ab91cbea7684e2e46d55ccfe6cb7
Mnigos <makowskiigor@gmail.com>
2022-01-11 14:47:32 +0100

    feat(index): add home page content

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,29 @@
 <template>
-  <div><h1>Hello World</h1></div>
+  <div class="flex justify-evenly w-full">
+    <header>
+      <!-- This is for SEO optimalization -->
+      <div class="hidden">
+        <h1>Biuro Rachunkowe</h1>
+        <p>Joanna Makowska</p>
+      </div>
+
+      <img src="/logo.png" alt="Logo Biuro Makowska" />
+    </header>
+
+    <main class="text-white flex flex-col justify-center gap-5 text-lg">
+      <h1 class="text-3xl">Witam na mojej stronie</h1>
+
+      <p>
+        Proponuję Państwu profesjonalne usługi księgowe<br />
+        w atrakcyjnych cenach z możliwością odbioru dokumentów od klienta.
+      </p>
+
+      <ul class="list-inside list-disc">
+        <p>Posiadam:</p>
+        <li>wieloletnie doświadczenie w obsłudze księgowej firm</li>
+        <li>licencję Ministra Finansów</li>
+        <li>ubezpieczenie w zakresie odpowiedzialności cywilnej</li>
+      </ul>
+    </main>
+  </div>
 </template>

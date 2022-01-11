9e23c479d0c24b8469832ceac0747f5e44a87827
Mnigos <makowskiigor@gmail.com>
2022-01-11 15:56:12 +0100

    feat(layout): add footer

---
+++ components/TheFooter.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const year = new Date().getFullYear()
+</script>
+
+<template>
+  <footer class="bg-accent p-2 flex justify-center">
+    <p>&copy; {{ year }} Igor Makowski</p>
+  </footer>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -5,15 +5,17 @@ const route = useRoute()
 
 <template>
   <div
-    class="bg-primary min-h-screen text-white"
+    class="bg-primary min-h-screen text-white flex flex-col justify-between"
     :style="route.path === '/' ? 'background-image: url(/background.jpg)' : ''"
   >
     <div class="flex justify-end">
-      <TheNavbar />
+      <the-navbar />
     </div>
 
     <div class="p-4 sm:p-8 md:p-16">
       <slot />
     </div>
+
+    <the-footer class="justify-self-end" />
   </div>
 </template>

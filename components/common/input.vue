7f53dde6f32d61e2658136812170be5d7cfc2308
Igor <makowskiigor@gmail.com>
2022-01-14 10:59:27 +0100

    feat(components): add `common-input` component

---
+++ components/common/input.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+import { generateId } from '~/utils'
+
+interface Props {
+  label: string
+}
+
+const { label } = defineProps<Props>()
+const id = generateId(label)
+</script>
+
+<template>
+  <label :for="id">
+    {{ label }}
+    <input :id="id" />
+  </label>
+</template>

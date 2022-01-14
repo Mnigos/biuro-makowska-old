cbd3533df4ec0d268f6864852fee066f445b6277
Igor <makowskiigor@gmail.com>
2022-01-14 11:01:16 +0100

    feat(components): add `common-button` component

---
+++ components/common/button.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+interface Props {
+  label: string
+}
+
+const { label } = defineProps<Props>()
+</script>
+
+<template>
+  <button>
+    {{ label }}
+  </button>
+</template>

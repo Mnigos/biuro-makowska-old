338f3df79daadacdd100fce02e2c77048c9f7163
Mnigos <makowskiigor@gmail.com>
2022-01-16 16:07:02 +0100

    feat(components): create `expand-card`

---
+++ components/ExpandCard.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { defineProps, ref, computed } from 'vue'
+
+import Button from './common/button.vue'
+
+interface Props {
+  title: string
+}
+
+const { title } = defineProps<Props>()
+
+const isExpanded = ref(false)
+const buttonLabel = computed(() => (isExpanded.value ? 'Zwiń' : 'Rozwiń'))
+</script>
+
+<template>
+  <section class="bg-primary p-8 rounded-2xl flex flex-col gap-8 shadow-2xl">
+    <header class="flex justify-between items-center">
+      <h1 class="text-3xl">{{ title }}</h1>
+
+      <Button :label="buttonLabel" @click="isExpanded = !isExpanded" />
+    </header>
+
+    <main v-if="isExpanded">
+      <div class="flex flex-col gap-8">
+        <slot />
+      </div>
+    </main>
+  </section>
+</template>

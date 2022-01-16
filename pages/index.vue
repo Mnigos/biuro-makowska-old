16d78f5b327b60cd4e7937acf66ff93823a263c9
Mnigos <makowskiigor@gmail.com>
2022-01-16 13:50:50 +0100

    feat(index): add second card

--- pages/index.vue
+++ pages/index.vue
@@ -26,8 +26,42 @@
       </the-card>
     </article>
 
-    <article>
-      <contact-form class="w-1/3" />
+    <article class="flex flex-col gap-16">
+      <section class="flex justify-end">
+        <the-card class="w-1/2" title="O Firmie">
+          <p>
+            Biuro Rachunkowe Joanna Makowska to certyfikowane biuro, które zapewnia swoim klientom
+            profesjonalną obsługę oraz rzetelne i fachowe świadczenie usług księgowych.
+          </p>
+          <p>
+            Wieloletnia praca zawodowa, nabyta wiedza teoretyczna i praktyczna oraz umiejętności jej
+            wykorzystania to dowód na rzetelną realizację usług.
+          </p>
+          <p>
+            Działalność biura rachunkowego to dziś istotna pomoc dla wielu przedsiębiorców. Chcemy
+            wychodzić naprzeciw wymaganiom i oczekiwaniom, które pojawiają się ze strony
+            przedsiębiorców, dlatego oferujemy szeroki zakres usług księgowych i kadrowych w ramach
+            prowadzenia KPiR, ksiąg rachunkowych, obsługi płac pracowniczych, deklaracji
+            podatkowych, ZUS, kontaktów z Urzędem Skarbowy, rozliczeń rocznych PIT oraz pomocy przy
+            działalności gospodarczej.
+          </p>
+          <p>
+            Świadczone przez nas usługi księgowe i kadrowe realizujemy na podstawie certyfikatu
+            księgowego wydanego przez Ministra Finansów nr 26183/01. Posiadamy obowiązkowe i
+            dodatkowe ubezpieczenie OC.
+          </p>
+          <p>
+            Jednocześnie stale poszerzamy swoją merytoryczną wiedzę uczestnicząc na bieżąco w
+            organizowanych szkoleniach, chociażby w Stowarzyszeniach Księgowych w Polsce. Serdecznie
+            zapraszamy Państwa do skorzystania z naszych kompleksowych i elastycznie dopasowanych do
+            Państwa potrzeb usług.
+          </p>
+        </the-card>
+      </section>
+
+      <section>
+        <contact-form class="w-1/3" />
+      </section>
     </article>
   </div>
 </template>

b2581208d67c1e96a54fa36416c2cb2248cdaafb
Mnigos <makowskiigor@gmail.com>
2022-01-16 19:20:31 +0100

    feat(components): create `contact-link`

---
+++ components/ContactLink.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+interface Props {
+  href: string
+}
+
+const { href } = defineProps<Props>()
+</script>
+
+<template>
+  <a :href="href" class="flex items-center gap-2 hover:text-accent transition-colors duration-300">
+    <slot />
+  </a>
+</template>

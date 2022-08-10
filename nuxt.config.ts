238dc836da4d6496aaee3c1cd399e99155f59f4d
Igor <makowskiigor@gmail.com>
2022-08-10 14:05:20 +0200

    feat(public): change favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,7 +7,25 @@ export default defineNuxtConfig({
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Biuro Rachunkowe Joanna Makowska' },
+      {
+        hid: 'description',
+        name: 'description',
+        content:
+          'Biuro Rachunkowe Joanna Makowska to certyfikowane biuro, które zapewnia swoim klientom profesjonalną obsługę oraz rzetelne i fachowe świadczenie usług księgowych.',
+      },
+      {
+        name: 'msapplication-TileColor',
+        content: '#000000',
+      },
+      {
+        name: 'theme-color',
+        content: '#ffffff',
+      },
+    ],
+    link: [
+      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+      { rel: 'icon', type: 'image/png', sizes: '64x64', href: '/favicon-64x64.png' },
+      { rel: 'manifest', href: '/site.webmanifest' },
     ],
   },
   buildModules: ['nuxt-windicss'],
